031ba9778b089f3786843122f3fa49f9e1578320
kilianthoraval <kthoraval@gmail.com>
2022-10-20 09:22:33 +0200

    p2

--- js/exo.js
+++ js/exo.js
@@ -1,1 +1,11 @@
-console.log('test 1...2...1...2');
+
+let hero = {
+    name : "Hercules",
+    job : "Demi-dieu",
+    age : 35,
+    department : 75,
+    arm : 60.5,
+    inRelationship : true, 
+}
+
+base.fillProfil(hero);
