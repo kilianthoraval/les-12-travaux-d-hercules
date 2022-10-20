4f86336b91c0a5a9293169e11968e7d607ed4375
kilianthoraval <kthoraval@gmail.com>
2022-10-20 09:59:40 +0200

    p4

--- js/exo.js
+++ js/exo.js
@@ -18,3 +18,13 @@ let friendsList = ["Jupiter", "Junon", "Alcmène", "Déjanire"]
 let bestFriend = friendsList[0];
 base.printFriends(friendsList);
 base.setBestFriend(bestFriend);
+
+// étape 3 : DOM
+
+let title = document.createElement("h1");
+title.classList.add = "banner__title";
+title.textContent = "Vous consultez le profil de Hercule";
+let herculeTitle = document.getElementById('header-banner');
+herculeTitle.append(title);
+
+
